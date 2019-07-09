4877041177eb40084f3d9abe8681355a71139ea4
greatforu <mankuugarg@gmail.com>
2019-07-09 14:35:24 +0530

    firebase file deleted

--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,36 @@
 import React from "react"
+import { Link } from "react-router-dom";
+
 
 
 class Main extends React.Component {
-    constructor() 
-    {
-         super()
-         this.state = {
-             country: 1
-         }
-           
-    }
-    render() {
-            
-        return (
-            <div></div>
 
-           
+    render() {
+
+        return (
+            <div>
+                <button>
+                    <h1><Link to="/Past">Past</Link></h1>
+                </button>
+
+                <button>
+                    <h1><Link to="/Upcoming">Upcoming</Link></h1>
+                </button>
+
+                <button>
+                    <h1><Link to="/Today">Today</Link></h1>
+                </button>
+
+
+                <button>
+                    <h1><Link to="/PointTable">PointTable</Link></h1>
+                </button>
+
+                <button>
+                    <h1><Link to="/Scorecard">scorecard</Link></h1>
+                </button>
+            </div>
         )
-}
+    }
 }
 export default Main
